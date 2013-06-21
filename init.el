8f471fd5a88eba2db4f3354438d4afac1ddb295c
Dana Pieluszczak <dana.pieluszczak@xls.xerox.com>
2013-06-21 11:43:33 -0400

    I don't want to see erc quit notifications either.

--- init.el
+++ init.el
@@ -233,7 +233,7 @@ use to determine if the package is installed/loaded."
   ;; modeline stuf
   (setq erc-track-exclude '("&bitlbee")
         erc-track-exclude-server-buffer t
-        erc-track-exclude-types '("JOIN" "PART")
+        erc-track-exclude-types '("JOIN" "PART" "QUIT")
         erc-track-shorten-start 5
         erc-track-shorten-cutoff 10)
   (erc-spelling-mode 1))
