e54ed013e8674cc4ea9a7a9e3a1d47a62f574963
Dana Pieluszczak <dana.pieluszczak@xls.xerox.com>
2014-04-21 09:45:55 -0400

    Check in things added by customize.
    I'm sick of seeing this in my unstaged changes.

--- init.el
+++ init.el
@@ -390,3 +390,18 @@ use to determine if the package is installed/loaded."
 ;; load additional local configuration if it exists
 (when (file-exists-p "~/.emacs.d/local.el")
   (load-file "~/.emacs.d/local.el"))
+
+;; --- everything below this line was probably added by customize ----
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
