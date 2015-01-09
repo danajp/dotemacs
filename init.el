d67fa54217aef09cd3bccd1be7e2a40504d524e8
Dana Pieluszczak <dana@greenhouse.io>
2015-01-09 13:25:33 -0500

    Bump font a little.

--- init.el
+++ init.el
@@ -40,7 +40,7 @@
   (setq browse-url-browser-function 'browse-url-default-browser))
 
 ;; gui stuff
-(add-to-list 'default-frame-alist '(font . "Terminus-11"))
+(add-to-list 'default-frame-alist '(font . "Terminus-12"))
 (add-to-list 'default-frame-alist '(mouse-wheel-mode . 1))
 (add-to-list 'default-frame-alist '(fringe-mode . 2))
 
