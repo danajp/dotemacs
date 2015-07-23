08b556651b8147534d107eeb71bb289e54b6de35
Dana Pieluszczak <dana@greenhouse.io>
2015-07-23 10:25:55 -0400

    Switch to solarized-dark theme.

--- init.el
+++ init.el
@@ -67,7 +67,7 @@ use to determine if the package is installed/loaded."
               '(lambda (f)
                  (with-selected-frame f
                    (when (window-system f)
-                     (color-theme-sanityinc-solarized-dark))))))
+                     (load-theme 'solarized-dark))))))
 
 ;; things I want in every file buffer
 (defun my-find-file-hook ()
@@ -484,9 +484,10 @@ use to determine if the package is installed/loaded."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (solarized-dark)))
  '(custom-safe-themes
    (quote
-    ("4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default))))
+    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
